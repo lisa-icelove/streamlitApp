a8ab7b166e43444f1c8cda7312ebc1a7d78974e3
Lisa <lisa1105ice@gmail.com>
2026-09-12 17:41:41 +0900

    a

--- pages/page1.py
+++ pages/page1.py
@@ -73,24 +73,42 @@ if st.button("0"):
     st.session_state.a=f"{st.session_state.a}{str(0)}"
     if st.session_state.number1 != None and st.session_state.number2 != "None":
         st.session_state.number2="None"
-
+#----------------------------------------------------------------
 if st.button("+"):
     st.session_state.q="+"
 
+#----------------------------------------------------------------
 if st.button("-"):
     st.session_state.q="-"
 
+#----------------------------------------------------------------
+if st.button("×"):
+    st.session_state.q="*"
+
+#----------------------------------------------------------------
+if st.button("÷"):
+    st.session_state.q="/"
+
+#----------------------------------------------------------------
 if st.button("符号を - に変更"):
+    st.session_state.a = st.session_state.a.replace("+", "")
     st.session_state.a="-" + st.session_state.a
 
+#----------------------------------------------------------------
 if st.button("符号を + に変更"):
+    st.session_state.a = st.session_state.a.replace("-", "")
     st.session_state.a="+" + st.session_state.a
+    
 #計算
 if st.button("計算"):
     if st.session_state.q=="+":
         st.write(int(st.session_state.number1) + int(st.session_state.number2))
     elif st.session_state.q=="-":
         st.write(int(st.session_state.number1) - int(st.session_state.number2))
+    elif st.session_state.q=="*":
+        st.write(int(st.session_state.number1) * int(st.session_state.number2))
+    elif st.session_state.q=="/":
+        st.write(int(st.session_state.number1) / int(st.session_state.number2))
 #表示
 st.write(f"現在入力中の値：{st.session_state.a}")
 st.write(f"現在の計算：{st.session_state.number1}{st.session_state.q}{st.session_state.number2}")
